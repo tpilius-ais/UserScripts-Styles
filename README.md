df402854d33f5277fd7b4bb46e986ff93cd1954f
Tim Pilius <tpilius@gmail.com>
2025-11-14 15:36:10 -0500

    ITGlue.user.js - v0.1.0. - Adds button that will copy a formatted link for Teams.  Will also rename the tab title to include the current document's title. ITGlue.user.css - v0.2.0 - Adding styling for the Teams button, and lightly highlights expired documents in the document list view

--- README.md
+++ README.md
@@ -4,17 +4,31 @@
 You're going to need the [Stylus](https://addons.mozilla.org/en-US/firefox/addon/styl-us/) and [Violentmonkey](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/) browser extensions in order to use these scripts.  Once the extensions are installed individual scripts/styles can be installed via clicking the associated install button below.
 
 <table>
-      <tr>
-      <!-- TODO add icon -->
+    <tr>
+        <!-- TODO add icon -->
         <td>ConnectWise Tweaks</td>
         <td>Overhauls the interface design of ConnectWise to improve visual clarity and usability.</td>
-        <td> <a href="https://github.com/tpilius-ais/UserScripts-Styles/raw/refs/heads/master/Styles/ConnectWise.user.css"><img alt="Static Badge" src="https://img.shields.io/badge/Install%20directly%20with-Stylus-00adad.svg"></a></td>
-      </tr>
-      <tr>
-      <!-- TODO add icon -->
+        <td></td>
+        <td> <a href="https://github.com/tpilius-ais/UserScripts-Styles/raw/refs/heads/master/Styles/ConnectWise.user.css" >
+                <img src="https://cdn.add0n.com/icons/stylus128.png" style="width:16px;height:16px;margin-right:6px;">
+                Install with Stylus
+            </a></td>
+    </tr>
+    <tr>
+        <!-- TODO add icon -->
         <td>IT Glue Tweaks</td>
-        <td>Overhauls the interface design of IT Glue to improve visual clarity and usability.</td>
-        <td> <a href="https://github.com/tpilius-ais/UserScripts-Styles/raw/refs/heads/master/Styles/ITGlue.user.css"><img alt="Static Badge" src="https://img.shields.io/badge/Install%20directly%20with-Stylus-00adad.svg"></a></td>
-      </tr>
-
+        <td>Overhauls the interface design of IT Glue to improve visual clarity and usability. Adds additional functionality to improve tech workflow</td>
+        <td>
+            <a href="https://github.com/tpilius-ais/UserScripts-Styles/raw/refs/heads/master/Scripts/ITGlue.user.js" >
+                <img src="https://violentmonkey.github.io/_astro/vm.C4h557K-.png" style="width:16px;height:16px;margin-right:6px;">
+                Install with Violentmonkey
+            </a>
+        <td>
+            <a href="https://github.com/tpilius-ais/UserScripts-Styles/raw/refs/heads/master/Styles/ITGlue.user.css" >
+                <img src="https://cdn.add0n.com/icons/stylus128.png" style="width:16px;height:16px;margin-right:6px;">
+                Install with Stylus
+            </a>
+        </td>
+    </tr>
 </table>
+
--- Scripts/ITGlue.user.js
+++ Scripts/ITGlue.user.js
@@ -1,16 +1,19 @@
 // ==UserScript==
-// @name         IT Glue
-// @version      2025-10-07
-// @description
-// @author       Tim Pilius
+// @name         IT Glue Tweaks
+// @author       tpilius-ais
+// @version      0.1.0
+// @description  // TODO
 // @match        https://ainfosys.itglue.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=myconnectwise.net
-// @grant        none
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=itglue.com
 // ==/UserScript==
 
-// TODO document + fix icon
+/* TODO look at adding these icons as replacements to the built in ones : https://www.iconexperience.com/g_collection/icons/# */
+// TODO document both here and on the readme
+// TODO Play around with replacing tags like [SPAM FILTER] with a color coded badge.
+// TODO https://github.com/gavsto/ITGlue-Helper
 'use strict';
 
+// Makes the tab title actually reflect the document title.
 function SetTabTitle()
 {
     if (document.title.includes("Documents - "))
@@ -23,6 +26,66 @@ function SetTabTitle()
     }
 }
 
+// Adds a button that will allow the user to easily copy a formatted link to use in Team, Notion, or elsewhere.
+// This link will be formatted so that the display text is of the format "[Client] - Document Title", while still linking to the correct page.
+function CreateCopyTeamsLinkButton()
+{
+    // This is only necessary on document pages.  Could possibly add it elsewhere too.
+    if (!window.location.href.includes("docs"))
+    {
+        return;
+    }
+
+    let pageHeaderContainer = document.querySelector(".react-page-header__left");
+    if (!pageHeaderContainer)
+    {
+        // Nothing to do since we couldn't find the header
+        return;
+    }
+
+    // Remove the old button and readd it.  This is so that it will always be to the right of the edit button.
+    let existingButton = pageHeaderContainer.querySelector('button.teams-button');
+    if (existingButton !== null)
+    {
+        existingButton.remove();
+    }
+
+    // Create the button
+    const copyBtn = document.createElement('button');
+    copyBtn.innerHTML = `<img src="https://upload.wikimedia.org/wikipedia/commons/0/07/Microsoft_Office_Teams_%282025%E2%80%93present%29.svg" alt="Teams"> Copy Link for Teams`;
+    copyBtn.classList.add("teams-button");
+
+    // Insert button after the div
+    pageHeaderContainer.appendChild(copyBtn);
+
+    // Add click event
+    copyBtn.addEventListener('click', () =>
+    {
+        const title = document.querySelector("h1.doc-title--view").innerText;
+        const html = `<html><body><a href="${window.location.href}">${GetFormattedOrgName()} - ${title}</a></body></html>`;
+
+        const blob = new Blob([html], { type: "text/html" });
+        const clipboardItem = new ClipboardItem({ "text/html": blob });
+        navigator.clipboard.write([clipboardItem]);
+    });
+}
+
+function GetFormattedOrgName()
+{
+    if (window.dvCurrent.organization.id === 5870388)
+    {
+        return "SBWDlaw";
+    }
+
+    return window.dvCurrent.organization.name;
+}
+
+function UpdateLogic()
+{
+    SetTabTitle();
+    CreateCopyTeamsLinkButton();
+}
+
 // Will try to update the title any time that it is changed on page navigation
-const observer = new MutationObserver(SetTabTitle);
+const observer = new MutationObserver(UpdateLogic);
 observer.observe(document.querySelector('title'), { childList: true });

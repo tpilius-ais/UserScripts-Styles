597316955e5554f529df13c99102c7fac0f4ec15
Tim Pilius <tpilius@gmail.com>
2026-03-24 09:50:16 -0400

    ITGlue.user.js - v0.2.0
    - When viewing a document or password, the client name will be prepended to the tab title.
    - Adds a Copy Link for Teams button on the passwords page.
    - Fixes an issue where the Teams link button won't work because of window.dvCurrent not being available.

--- Scripts/ITGlue.user.js
+++ Scripts/ITGlue.user.js
@@ -1,28 +1,46 @@
 // ==UserScript==
 // @name         IT Glue Tweaks
 // @author       tpilius-ais
-// @version      0.1.0
+// @version      0.2.0
 // @description  // TODO
 // @match        https://ainfosys.itglue.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=itglue.com
 // ==/UserScript==
-
-/* TODO look at adding these icons as replacements to the built in ones : https://www.iconexperience.com/g_collection/icons/# */
 // TODO document both here and on the readme
 // TODO Play around with replacing tags like [SPAM FILTER] with a color coded badge.
-// TODO https://github.com/gavsto/ITGlue-Helper
 'use strict';
 
-// Makes the tab title actually reflect the document title.
+// Cleans up the tab title so it is actually more readable and useful.
 function SetTabTitle()
 {
+    // Makes it so that the document name is actually readable in the tab title, and not just cut off because "Documents -" at the beginning.
     if (document.title.includes("Documents - "))
     {
-        document.title = document.title.replace("Documents - ", "");
+        const orgName = GetFormattedOrgName();
+        document.title = document.title.replace("Documents - ", `${orgName} - `);
     }
+    // Removes IT Glue from the tab since I already know what site I'm on.
     if (document.title.includes(" — IT Glue"))
     {
         document.title = document.title.replace(" — IT Glue", "");
+    }
+
+    // Prepends the client name to these pages
+    // TODO figure out if this could be more generic so it applies to all of the pages
+    if (document.title === "Documents")
+    {
+        const orgName = GetFormattedOrgName();
+        document.title = `${orgName} - Documents`;
+    }
+    if (document.title === "Passwords")
+    {
+        const orgName = GetFormattedOrgName();
+        document.title = `${orgName} - Passwords`;
+    }
+    if (document.title === "Printing")
+    {
+        const orgName = GetFormattedOrgName();
+        document.title = `${orgName} - Printing`;
     }
 }
 
@@ -30,7 +48,7 @@ function SetTabTitle()
 // This link will be formatted so that the display text is of the format "[Client] - Document Title", while still linking to the correct page.
 function CreateCopyTeamsLinkButton()
 {
-    // This is only necessary on document pages.  Could possibly add it elsewhere too.
+    // This is only necessary on document pages.
     if (!window.location.href.includes("docs"))
     {
         return;
@@ -52,6 +70,7 @@ function CreateCopyTeamsLinkButton()
 
     // Create the button
     const copyBtn = document.createElement('button');
+    // TODO swap this over to a the copy hosted in this repo
     copyBtn.innerHTML = `<img src="https://upload.wikimedia.org/wikipedia/commons/0/07/Microsoft_Office_Teams_%282025%E2%80%93present%29.svg" alt="Teams"> Copy Link for Teams`;
     copyBtn.classList.add("teams-button");
 
@@ -70,20 +89,99 @@ function CreateCopyTeamsLinkButton()
     });
 }
 
-function GetFormattedOrgName()
+function CreateCopyTeamsLinkButton_Passwords()
 {
-    if (window.dvCurrent.organization.id === 5870388)
+    // Only creating this button on the passwords page
+    if (!window.location.href.includes("passwords"))
     {
-        return "SBWDlaw";
+        return;
     }
 
-    return window.dvCurrent.organization.name;
+    let sidebarButtonsContainer = document.querySelector(".sidebar-buttons-section .buttons");
+    if (!sidebarButtonsContainer)
+    {
+        return;
+    }
+
+    // Remove the old button and readd it.  This is so that it will always be to the right of the edit button.
+    let existingButton = sidebarButtonsContainer.querySelector('button.teams-button');
+    if (existingButton !== null)
+    {
+        existingButton.remove();
+    }
+
+    // Create the button
+    const copyBtn = document.createElement('button');
+    // TODO swap this over to the copy hosted in this repo
+    copyBtn.innerHTML = `<img src="https://upload.wikimedia.org/wikipedia/commons/0/07/Microsoft_Office_Teams_%282025%E2%80%93present%29.svg" alt="Teams"> Copy Link for Teams`;
+    copyBtn.classList.add("teams-button");
+
+    // Insert button after the div
+    sidebarButtonsContainer.appendChild(copyBtn);
+
+    // Add click event
+    copyBtn.addEventListener('click', () =>
+    {
+        const title = document.querySelector(".page-header.qa-page-header.h1").innerText;
+        const html = `<html><body><a href="${window.location.href}">${GetFormattedOrgName()} - ${title}</a></body></html>`;
+
+        const blob = new Blob([html], { type: "text/html" });
+        const clipboardItem = new ClipboardItem({ "text/html": blob });
+        navigator.clipboard.write([clipboardItem]);
+    });
 }
 
+// Takes Organization names that are very long and formats them down into acronyms,
+// so that the org name doesn't hide the document title in the tab text.
+function GetFormattedOrgName()
+{
+    const orgMap = {
+        5185248: "AIS",
+        5524967: "CCA",
+        5198997: "Engel Law",
+        6038230: "FFCU",
+        6213647: "FMDT",
+        5199266: "Ingerman",
+        5198977: "Naiman",
+        5199276: "PK Law",
+        5870388: "SBWDlaw",
+        5199304: "Waranch",
+        5198991: "WEA"
+    };
+
+    const orgId = unsafeWindow.dvCurrent.organization.id;
+    return orgMap[orgId] ?? unsafeWindow.dvCurrent.organization.name;
+}
+
+// Icons taken from https://www.iconexperience.com/g_collection/icons
+// TODO play around with this some more.  Need to add a delay or something for it to pickup the elements.
+function ReplaceFolderIcons()
+{
+    console.log("Found elements: ");
+    console.log(document.querySelectorAll('.doc-name i.item-name-icon.fa-folder'));
+    document.querySelectorAll('.doc-name i.item-name-icon.fa-folder').forEach(icon =>
+    {
+        const img = document.createElement('img');
+        // TODO change the url to point to the repo's copy
+        img.src = 'https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/16x16/folder.png';
+        img.alt = 'Folder';
+        img.style.verticalAlign = 'middle';
+        img.style.marginRight = '6px';
+        img.style.height = '16px';
+        img.style.width = '16px';
+
+        // Insert the new image before removing the old icon
+        icon.parentNode.insertBefore(img, icon);
+        icon.remove();
+    });
+}
+
+// TODO comment
 function UpdateLogic()
 {
     SetTabTitle();
     CreateCopyTeamsLinkButton();
+    CreateCopyTeamsLinkButton_Passwords();
 }
 
 // Will try to update the title any time that it is changed on page navigation
